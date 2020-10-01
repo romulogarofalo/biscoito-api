525b168a5eaed9960c2a1d51afe7435b1b9cde5f
romulo garofalo <romulogarofalo@gmail.com>
2020-10-01 05:18:37 +0000

    :key: added config for local database

--- config/dev.exs
+++ config/dev.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 # Configure your database
 config :biscoito, Biscoito.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "biscoito_dev",
+  username: "root",
+  password: "root",
+  database: "biscoito",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
