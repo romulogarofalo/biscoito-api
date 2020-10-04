b9df52e8a6f4bfefad4b4bcf3e0bc3d6a58f3bdf
romulo garofalo <romulogarofalo@gmail.com>
2020-10-04 10:52:25 +0000

    :construction: get the id of the user

--- lib/biscoito_web/controllers/lists_controller.ex
+++ lib/biscoito_web/controllers/lists_controller.ex
@@ -3,13 +3,16 @@ defmodule BiscoitoWeb.ListsController do
 
   action_fallback BiscoitoWeb.FallbackController
 
+
   def create(%{req_headers: headers} = conn, attr_list) do
-    headers
-    |> Enum.at(1)
-    |> elem(1)
-    |> Biscoito.Guardian.decode_and_verify()
-    |> IO.inspect()
+    bearer_token = elem(Enum.at(headers, 1), 1)
+    token = String.slice(bearer_token, 7..String.length(bearer_token))
+    {:ok, paramsToken} = Biscoito.Guardian.decode_and_verify(token, %{"typ" => "access"})
+    %{"sub" => idUser} = paramsToken
     with {:ok, list} <- Biscoito.create_list(attr_list) do
+      IO.inspect(list.id)
+      # Biscoito.create_users_lists()
+
       conn
       |> put_status(:created)
       |> render("list.json", list: list)
